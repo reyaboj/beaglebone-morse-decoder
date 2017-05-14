d0f364f46f3c3351538beeb1d94a34d40460d4b0
Md Istiaque Al Jobayer <mial2@student.monash.edu>
2017-05-14 16:58:46 +0000

    Update server description

--- src/server.js
+++ src/server.js
@@ -3,10 +3,40 @@
 
    Client-server architecture with the following responsibilities for each host:
      - Server
-       Read signals from the motion sensor and decode signals as Morse code.
+       Read signals from the motion sensor and decodes signals as Morse code.
        Push message to Firebase as it gets decoded.
 
      - Client
-       Thin client simply listens to Firebase for updates and displays the
-       decoded message to the user.
+       Listens to Firebase for updates and displays the decoded message to the user.
  */
+
+const five = require('johnny-five');
+const bbio = require('beaglebone-io');
+const beagle = new five.Board({
+    io: new bbio(),
+    repl: false
+});
+
+beagle.on('ready', function () {
+    const SENSOR_PIN = 'P8_8';  // PIR sensor output signal pin
+    const POLL_FREQ = 1000;  // polling frequency in milliseconds
+
+    const sensor = new five.Motion({
+        pin: SENSOR_PIN,
+        freq: POLL_FREQ
+    });
+
+    sensor.on('calibrated', function () {
+        console.log(
+            `[Calibrated]\n`
+                + `Pin = ${SENSOR_PIN}\n`
+                + `Polling Rate = every ${POLL_FREQ} msecs\n`
+        );
+
+        sensor.on('data', function (data) {
+            console.log(
+                `Timestamp{${data.timestamp}}  ${data.detectedMotion ? 'HIGH' : 'LOW'}`
+            );
+        });
+    });
+});
